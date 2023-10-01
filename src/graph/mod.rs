fd669d5edb237875e1c032cdc39ccdb3a32af4d9
Huang Geyang <Sukebeta@outlook.com>
2023-10-01 11:57:51 +0800

    Reimplement Node structure

--- src/graph/mod.rs
+++ src/graph/mod.rs
@@ -4,49 +4,53 @@ use std::fmt::{Display, Formatter};
 mod adjacencies;
 mod flow;
 
-// An edge from the capacity network is
+// An edge from the capacity network is represented as:
 // from, token, to -> capacity
 //
-// In the transformation into the flow network, we add two intermediate nodes
-// per edge that are potentially shared with other edges:
+// In the transformation into the flow network, two intermediate nodes are added
+// per edge that might be shared with other edges:
 //
-// from -A-> BalanceNode(from, token) -B-> TrustNode(to, token) -C-> to
+// from -A-> BalanceNode(from) -B-> TrustNode(to, token) -C-> to
 //
-// The capacities (A, B, C) are as follows:
-// A: the max of all capacity-netwok edges of the form (from, token, *), or A's balance of "token" tokens.
-// B: the actual capacity of the capacity-network edge (from, token, to), or the "send limit" from "from" to "to" in "token" tokens
+// The capacities (A, B, C) are defined as:
+// A: the max of all capacity-network edges of the form (from, token, *) or A's balance of "token" tokens.
+// B: the actual capacity of the capacity-network edge (from, token, to) or the "send limit" from "from" to "to" in "token" tokens.
 // C: if "token" is C's token (this is a "send to owner" edge): infinity or the sum of all incoming edges.
 //    otherwise: the max of all capacity-network edges of the form (*, token, to) or the trust limit of "to" for "token" tokens.
 
 #[derive(Debug, Eq, PartialEq, Hash, Clone, PartialOrd, Ord)]
-pub enum Node {
-    Node(Address),
-    BalanceNode(Address, Address),
-    TrustNode(Address, Address),
+pub enum Node<'a> {
+    /// Represents a trust relationship between two addresses for a specific token.
+    TrustNode(&'a Address, &'a Address),
+    /// Represents the balance of an address.
+    BalanceNode(&'a Address),
 }
 
-pub fn node_as_address(node: &Node) -> &Address {
-    if let Node::Node(address) = node {
-        address
-    } else {
-        panic!()
+/// Retrieves the address from a node.
+/// For a BalanceNode, it returns the associated address.
+/// For a TrustNode, it returns the "to" address.
+pub fn node_as_address<'a>(node: &Node<'a>) -> &'a Address {
+    match node {
+        Node::BalanceNode(address) => address,
+        Node::TrustNode(to, _) => to,
     }
 }
 
-pub fn as_trust_node(node: &Node) -> (&Address, &Address) {
+/// Extracts the addresses from a TrustNode.
+/// Returns a tuple containing the "to" address and the token address.
+pub fn as_trust_node<'a>(node: &Node<'a>) -> (&'a Address, &'a Address) {
     if let Node::TrustNode(to, token) = node {
         (to, token)
     } else {
-        panic!()
+        panic!("Expected a TrustNode variant")
     }
 }
 
-impl Display for Node {
+impl<'a> Display for Node<'a> {
     fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
         match self {
-            Node::Node(address) => write!(f, "{address}"),
-            Node::BalanceNode(from, token) => write!(f, "(bal {from} x {token})"),
-            Node::TrustNode(to, token) => write!(f, "(trust {to} x {token})"),
+            Node::BalanceNode(address) => write!(f, "{}", address),
+            Node::TrustNode(to, token) => write!(f, "(trust {} x {})", to, token),
         }
     }
 }
